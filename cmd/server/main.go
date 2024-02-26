5fffc7ef59f353d3a62f277c467c5df2401d1e1d
xxaidazameticaxx <azametica1@etf.unsa.ba>
2024-02-27 00:44:20 +0100

    Make cmd/server act as an entry point only

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,49 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/OAuth2withJWT/identity-provider/server"
 )
 
-type User struct {
-	Email    string
-	Username string
-	Password string
-}
-
-func RegistrationFormHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("views/registration.html")
-	err := tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func RenderingRegistrationDetails(w http.ResponseWriter, r *http.Request) {
-	registrationDetails := User{
-		Email:    r.FormValue("email"),
-		Username: r.FormValue("username"),
-		Password: r.FormValue("password"),
-	}
-
-	_ = registrationDetails
-
-	tmpl, _ := template.ParseFiles("views/registration.html")
-	err := tmpl.Execute(w, struct{ Success bool }{true})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/registration", RegistrationFormHandler).Methods("GET")
-	r.HandleFunc("/registration", RenderingRegistrationDetails).Methods("POST")
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	s := server.New()
+	log.Fatal(s.Run())
 }
