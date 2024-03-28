0768774c6021cc1d655370012c86db3be43c2bde
xxaidazameticaxx <azametica1@etf.unsa.ba>
2024-03-29 00:22:06 +0100

    Update db scheme

--- db/scheme.sql
+++ db/scheme.sql
@@ -7,9 +7,12 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TYPE session_status AS ENUM ('active', 'inactive');
+
 CREATE TABLE IF NOT EXISTS sessions (
     id SERIAL PRIMARY KEY,
     session_id VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    expires_at TIMESTAMP NOT NULL
+    expires_at TIMESTAMP NOT NULL,
+    status session_status DEFAULT 'active'
 );
