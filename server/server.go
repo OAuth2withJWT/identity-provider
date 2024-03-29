ae892df51c82ddfc22c62c276895f027f11951e3
lheleg <lheleg1@etf.unsa.ba>
2024-03-29 22:43:17 +0100

    Modify rendering registration details logic

--- server/server.go
+++ server/server.go
@@ -22,8 +22,12 @@ func New(a *app.Application) *Server {
 }
 
 func (s *Server) Run() error {
+	s.router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+
 	s.router.HandleFunc("/registration", RegistrationFormHandler).Methods("GET")
+	s.router.HandleFunc("/login", LoginFormHandler).Methods("GET")
 	s.router.HandleFunc("/registration", s.RenderingRegistrationDetails).Methods("POST")
+	s.router.HandleFunc("/", HomePageHandler).Methods("GET")
 
 	log.Println("Server started on port 8080")
 	return http.ListenAndServe(":8080", s.router)
@@ -38,27 +42,53 @@ func RegistrationFormHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("views/index.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func LoginFormHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("views/login.html")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func (s *Server) RenderingRegistrationDetails(w http.ResponseWriter, r *http.Request) {
 	req := app.CreateUserRequest{
 		FirstName: r.FormValue("firstName"),
 		LastName:  r.FormValue("lastName"),
 		Email:     r.FormValue("email"),
 		Username:  r.FormValue("username"),
-	}
-	user, err := s.app.UserService.Create(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		Password:  r.FormValue("password"),
 	}
 
-	tmpl, _ := template.ParseFiles("views/registration.html")
-	err = tmpl.Execute(w, struct {
-		Success  bool
-		Username string
-	}{true, user.Username})
-
+	_, err := s.app.UserService.Create(req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		var errorMessage string
+		if fieldErr, ok := err.(*app.FieldError); ok {
+			errorMessage = fieldErr.Message
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data := struct {
+			ErrorMessage string
+		}{ErrorMessage: errorMessage}
+
+		tmpl, _ := template.ParseFiles("views/registration.html")
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
+
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
