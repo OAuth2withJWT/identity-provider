b6417550ff9d765f17f0b6c44ec50baf38f8e447
xxaidazameticaxx <azametica1@etf.unsa.ba>
2024-05-23 18:34:33 +0200

    Add /auth endpoint to the server

---
+++ server/auth.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func (s *Server) handleAuth(w http.ResponseWriter, r *http.Request) {
+	responseType := r.URL.Query().Get("response_type")
+	clientID := r.URL.Query().Get("client_id")
+	redirectURI := r.URL.Query().Get("redirect_uri")
+	scope := r.URL.Query().Get("scope")
+	state := r.URL.Query().Get("state")
+
+	if clientID == "" || redirectURI == "" || responseType != "code" {
+		http.Error(w, "invalid_request", http.StatusBadRequest)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("views/consent-screen.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//login
+
+	//consent
+
+	//authorizationCode, _ := s.app.ClientService.GenerateAuthorizationCode(clientID, redirectURI, scope)
+
+	//http.Redirect(w, r, redirectURI+"?code="+authorizationCode+"&state="+state, http.StatusFound)
+}
