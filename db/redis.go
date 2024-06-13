f47c0187eba2f84a7d88aef282da78508363a63c
xxaidazameticaxx <azametica1@etf.unsa.ba>
2024-06-13 22:23:57 +0200

    Add to .env variables

--- db/redis.go
+++ db/redis.go
@@ -1,13 +1,28 @@
 package db
 
 import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
 func ConnectRedis() *redis.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbNum, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		log.Fatalf("Invalid REDIS_DB value: %v", err)
+	}
+
 	return redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       dbNum,
 	})
 }
